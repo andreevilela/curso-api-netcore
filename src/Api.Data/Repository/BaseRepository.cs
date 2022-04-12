2fc7ac0638df5ab14023363dbae01aba8e334b0e
andrecavalcantezup <andre.cavalcante@zup.com.br>
2022-04-12 18:14:12 -0300

    Base Repository SelectAsync

--- src/Api.Data/Repository/BaseRepository.cs
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -57,14 +57,29 @@ namespace Api.Data.Repository
             return await _dataset.AnyAsync(p => p.Id.Equals(id));
         }
 
-        public Task<T> SelectAsync(Guid id)
+        public async Task<T> SelectAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(id));
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
-        public Task<IEnumerable<T>> SelectAsync()
+        public async Task<IEnumerable<T>> SelectAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                
+                throw e;
+            }
         }
 
         public async Task<T> UpdateAsync(T item)
